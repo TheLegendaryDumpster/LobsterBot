29a9250b6f16228b0f6bd357b2482f06199e43ee
TRASH <schoolishell100@gmail.com>
2023-01-28 16:00:24 -0600

    Add website to /about

--- commands/tools/embed.js
+++ commands/tools/embed.js
@@ -7,7 +7,7 @@ module.exports = {
     async execute(interaction, client) {
         const embed = new EmbedBuilder()
             .setTitle('About lobster_bot')
-            .setDescription('Lobster_bot is a simple free and open source discord bot')
+            .setDescription('Lobster_bot is a simple free and open source discord bot\n[Website](https://thelegendarydumpster.github.io/LobsterBot/)')
             .setColor(0x53a4f5)
         
         await interaction.reply({
