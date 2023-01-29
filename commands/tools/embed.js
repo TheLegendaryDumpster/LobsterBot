e2821d5342e03327a589ec22e3caa8b499ca9055
james56576 <101009316+james56576@users.noreply.github.com>
2023-01-29 05:18:44 +0000

    changed about command

--- commands/tools/embed.js
+++ commands/tools/embed.js
@@ -7,9 +7,9 @@ module.exports = {
     async execute(interaction, client) {
         const embed = new EmbedBuilder()
             .setTitle('About lobster_bot')
-            .setDescription('Lobster_bot is a simple free and open source discord bot\n[Website](https://thelegendarydumpster.github.io/LobsterBot/)\n[Support Server](https://discord.gg/azalea-essentials)')
+            .setDescription('Lobster_bot is a simple free and open source discord bot.\n\n------------------------Commands------------------------\n/about ¦ You just did it.\n/allow-pings ¦ Set if you want to be pinged or not.\n/credits ¦ Show people who made the bot.\n/ping ¦ See the bots ping.\n/say ¦ Make the bot say things.')
             .setColor(0x53a4f5)
-        
+                             
         await interaction.reply({
             embeds: [embed]
         })
