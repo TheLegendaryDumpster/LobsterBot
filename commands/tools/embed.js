4015012a74139875d903ad54e063caa27c2ba723
TheLegendaryDumpster <123837611+TheLegendaryDumpster@users.noreply.github.com>
2023-01-28 23:35:48 -0600

    Things

--- commands/tools/embed.js
+++ commands/tools/embed.js
@@ -7,7 +7,7 @@ module.exports = {
     async execute(interaction, client) {
         const embed = new EmbedBuilder()
             .setTitle('About lobster_bot')
-            .setDescription('Lobster_bot is a simple free and open source discord bot.\n\n------------------------Commands------------------------\n/about ¦ You just did it.\n/allow-pings ¦ Set if you want to be pinged or not.\n/credits ¦ Show people who made the bot.\n/ping ¦ See the bots ping.\n/say ¦ Make the bot say things.')
+            .setDescription('Lobster_bot is a simple free and open source discord bot.\n\n------------------------Commands------------------------\n</about:0> You just did it.\n</allow-pings:0> Set if you want to be pinged or not.\n</credits:0> Show people who made the bot.\n</ping:0> See the bots ping.\n</say:0> Make the bot say things.')
             .setColor(0x53a4f5)
                              
         await interaction.reply({
