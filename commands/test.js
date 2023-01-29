2424caf5a67a06a3b380b409dd234ec67732a9a7
james56576 <101009316+james56576@users.noreply.github.com>
2023-01-29 04:38:33 +0000

    testing

---
+++ commands/test.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("testtitle")
+        .setDescription("test")
+        .addStringOption(option=>
+            option.setName("string")
+                .setDescription("TEST")
+                .setRequired(true)),
+    async execute(interaction, client, fns) {
+        interaction.reply(`u inputted ${string.value}`)
+    }
+}
